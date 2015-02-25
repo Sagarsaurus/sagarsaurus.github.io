c6e73e1a49b5af0e1a00439fa787a3ed27c32f9e
Sagarsaurus <sagrsccr@gmail.com>
2015-02-25 04:24:00 -0500

    Decided not to be dumb and completed theme change

--- calculator.js
+++ calculator.js
@@ -113,6 +113,7 @@ function changeTheme(button) {
             x[i].style.fontFamily = "Times New Roman";
         }
         document.getElementById("window").style.background="green";
+        document.getElementById("name").style.background="green";
         this.themeState=1;
     }
     else if(this.themeState==1 && this.backgroundState==0 && this.buttonBackgroundState==0) {
@@ -125,6 +126,8 @@ function changeTheme(button) {
         for (var i = 0; i < x.length; i++) {
             x[i].style.fontFamily = "Arial";
         }
+        document.getElementById("window").style.background="black";
+        document.getElementById("name").style.background="black";
         this.themeState=0;
     }
 }
